db74f00434d2600be92435cc44f5ef2defb0c465
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-01-23 00:07:32 +0900

    非同期処理のサンプル

--- chapter1/index.js
+++ chapter1/index.js
@@ -12,6 +12,25 @@ function myReduce() {
 }
 myReduce();
 
+function myPromise() {
+    const promise = new Promise((resulve, reject) => {
+        resulve()
+    })
+    const inCaseOfSuccess = () => {
+        console.log("非同期の処理が成功し、resolveが通知された！")
+    }
+    const inCaseOfFailure = () => {
+        console.log("非同期の処理が失敗し、rejectが通知された。。。")
+    }
+    //resoleve()で非同期処理が成功したという通知を受け取り、
+    //then()で成功し場合の処理を実行
+    promise
+        .then(inCaseOfSuccess, inCaseOfFailure)
+        .then(inCaseOfSuccess, inCaseOfFailure)
+        .then(inCaseOfSuccess, inCaseOfFailure)
+        .catch(inCaseOfFailure)
+}
+myPromise()
 console.log(sampleVariable);
 console.log(sampleFunc());
 console.log(App())
