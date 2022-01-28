80b56720b11d5a1067fbf604d7a98cb63fcbe68e
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-01-28 23:44:17 +0900

    useEffectのサンプルコード

--- chapter5/hooks-tutorial/src/App.js
+++ chapter5/hooks-tutorial/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 import { Counter } from "./Counter";
@@ -6,6 +6,41 @@ import { Hello } from "./Hello";
 
 const INITIAL_COUNT = 0;
 const INITIAL_NAME = "JavaScript";
+
+const Timer = () => {
+  //constは状態変数/現在の値
+  const [count, setCount] = useState(INITIAL_COUNT);
+  const countReset = () => {
+    setCount(INITIAL_COUNT);
+  };
+
+  const countIncrement = () => {
+    //現在のcountを引数で受け取ることができる
+    setCount((prevCount) => prevCount + 1);
+    console.log('カウントアップ + 1');
+  };
+
+  const callbackFunction = () => {
+    //副作用関数が実行されるとalertdで「副作用関数が実行されました！」と表示される
+    alert('副作用関数が実行されました！');
+    const timer = setInterval(countIncrement, 1000);
+    return () => {
+      console.log('timerが削除されました。');
+      clearInterval(timer);
+    };
+  };
+
+  //第二引数にからの依存配列を渡しているので
+  //初回レンダリング時にのみ副作用関数が実行される
+  useEffect(callbackFunction, []);
+
+  return (
+    <div className="App">
+      <p>現在のカウント数：{count}</p>
+      <button onClick={countReset}>RESET</button>
+    </div>
+  );
+};
 
 export default function App() {
   const [count, setCount] = useState(INITIAL_COUNT);
@@ -28,6 +63,16 @@ export default function App() {
     return setName(e.target.value);
   };
 
+  //useEffect
+  const callbackFunction = () => {
+    document.title = (`count, ${count}, name ${name}副作用関数が実行されました。　!`);
+    return () => {
+      console.log("クリーンアップ関数が実行されました。");
+    };
+  }
+  useEffect(callbackFunction, [count, name]);
+  const [display, toggleDisplay] = useState(false);
+  const handleToggleDisplay = () => { toggleDisplay(!display); };
   return (
     <>
       <Counter
@@ -42,6 +87,11 @@ export default function App() {
         handleChangeName={handleChangeName}
         initialName={INITIAL_NAME}
       />
+      {/* ボタンをクリックするとhandleToggleDisplay関数が実行される */}
+      <button onClick={handleToggleDisplay}>
+        {display ? "タイマーを非表示" : "タイマーを表示"}
+      </button>
+      {display && <Timer />}
     </>
   );
 }
