f5d251cbc5586ca8e45c27b55284d7b1ed3a1e98
OtsukaTomoaki <ootsuka.ootsuka.ootsuka.26@gmail.com>
2022-01-29 13:30:04 +0900

    Memoの利用

--- chapter5/hooks-tutorial/src/App.js
+++ chapter5/hooks-tutorial/src/App.js
@@ -22,7 +22,7 @@ const Timer = () => {
 
   const callbackFunction = () => {
     //副作用関数が実行されるとalertdで「副作用関数が実行されました！」と表示される
-    alert('副作用関数が実行されました！');
+    console.log('副作用関数が実行されました！');
     const timer = setInterval(countIncrement, 1000);
     return () => {
       console.log('timerが削除されました。');
@@ -50,18 +50,43 @@ const CountResult = React.memo(({text, countState}) => {
   //Counterコンポーネント（親）のボタンがクリックされて
   //Counterコンポーネント（親）のもつcountStateが更新されたら
   //CountResultコンポーネント（子）が再レンダリングされる
-  console.log(`${text}ボタンがクリックされました！`);
+  console.log(`${text}ボタンがクリックされました！`);//Memo化しない場合はA, Bどちらのボタンがクリックされても再描画されるので、コンソールに書き込まれる
   return (
     <p>
       {/* ボタン名とCounterコンポーネント（親）からpropsで受け取った現在のcountStateを表示 */}
       {text}: {countState}
     </p>
   );
-})
+});
 
+//Counterコンポーネント（親）を作成
+const CounterMemo = () => {
+  //状態変数 countStateAとcountStateBの初期値は０
+  const [countStateA, setCountStateA] = useState(0);
+  const [countStateB, setCountStateB] = useState(0);
 
+  //Aボタンのstateセット用countIncrementA関数、現在のcountStateAを引数で受け取ることができる
+  const countIncrementA = () => setCountStateA((prevCountStateA) => prevCountStateA + 1);
+
+  //Bボタンのstateセット用
+  const countIncrementB = () => setCountStateB((prevCountStateB) => prevCountStateB + 1);
+  
+  return (
+    <>
+      {/* 現在のcountStateA */}
+      <CountResult text="Aボタン" countState={countStateA} />
+      {/* 現在のcountStateA */}
+      <CountResult text="Bボタン" countState={countStateB} />
+      {/* Aボタン */}
+      <button onClick={countIncrementA}>A ボタン</button>
+      {/* Bボタン */}
+      <button onClick={countIncrementB}>B ボタン</button>
+    </>
+  );
+};
 
 export default function App() {
+  //useEffect
   const [count, setCount] = useState(INITIAL_COUNT);
 
   const [name, setName] = useState(INITIAL_NAME);
@@ -92,6 +117,8 @@ export default function App() {
   useEffect(callbackFunction, [count, name]);
   const [display, toggleDisplay] = useState(false);
   const handleToggleDisplay = () => { toggleDisplay(!display); };
+
+  //useMemo
   return (
     <>
       <Counter
@@ -111,6 +138,8 @@ export default function App() {
         {display ? "タイマーを非表示" : "タイマーを表示"}
       </button>
       {display && <Timer />}
+
+      <CounterMemo />
     </>
   );
 }
